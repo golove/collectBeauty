8266cee762e3f9846f437386774b612c5562e11c
golove <liuziyu7@gmail.com>
2024-10-30 22:41:25 +0800

    add click event

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 async function init() {
     let data = await loadData();
+    const modal = document.getElementById('modal');
+   
     const app = document.getElementById('app');
+
     const containerWidth = app.clientWidth;
     // console.log(containerWidth)
     // const cols = 4;
@@ -17,7 +20,44 @@ async function init() {
         const cardNodeList = [];
         dataArray.forEach(items => {
         items.forEach(item => {
-           const card = createCard(item, cardWidth)
+           const card = createCard(item, cardWidth,true)
+           card.addEventListener('click',()=>{
+            const modalClose = document.createElement('div')
+            modalClose.setAttribute('id','modal-close') //
+            modalClose.style.position = 'absolute'
+            modalClose.style.top = '0'
+            modalClose.style.right = '0'
+            modalClose.style.zIndex = '10'
+             modalClose.innerText = 'Close'
+             modal.append(modalClose)
+            // modal.style.height = '100%'
+            modal.style.display = 'block'
+            const imgNodeList = []
+            imgArray = generateTwoDimensionalArray(item.srcs, cols, cardWidth, mrLeft, gap,(e)=>{return (cardWidth / e.aspect_ratio) + gap})
+            imgArray.forEach(imgs => {
+                imgs.forEach(arr => {
+                    const img = createCard(arr, cardWidth,false)
+                    imgNodeList.push(img)
+                })
+            
+            })
+           modal.append(...imgNodeList)
+           modalClose.addEventListener('click',()=>{
+            // modal.style.height = '0'
+            modal.style.display = 'none'
+            modal.innerHTML = ''
+           })
+           // 判断app有滚动操作
+           window.addEventListener('scroll',()=>{
+                modal.style.display = 'none'
+               modal.innerHTML = ''
+           })
+
+           window.addEventListener('resize',()=>{
+               debounce(appResize(imgNodeList),300)
+           })
+          
+        })
            cardNodeList.push(card)
          })
 
@@ -67,7 +107,7 @@ function updateCardPosition(cards, cols, cardWidth, mrLeft, gap) {
 }
 
 function generateTwoDimensionalArray(array, cols, cardWidth, mrLeft, gap, heightFunc) {
-    const newArray = new Array(cols).fill(0);
+    const newArray = new Array(cols).fill(gap);
     const dataArray = new Array(cols).fill(null).map(() => []);
     let minIndex = 0;
     let minHeight = newArray[0];  // 初始最小高度
@@ -96,33 +136,42 @@ function generateTwoDimensionalArray(array, cols, cardWidth, mrLeft, gap, height
 }
 
 
-function createCard(item, cardWidth) {
+function createCard(item, cardWidth,isTitle=false) {
     const card = document.createElement('div');
     card.setAttribute('class', 'card');
-    card.setAttribute('style', `position:absolute;left: ${item.x}px;top: ${item.y}px;width: ${cardWidth}px;height: ${cardWidth / item.srcs[0].aspect_ratio}px`);
-
-    const title = document.createElement('h3');
+    const img = document.createElement('img');
+    if(item.srcs){
+        card.setAttribute('style', `position:absolute;left: ${item.x}px;top: ${item.y}px;width: ${cardWidth}px;height: ${cardWidth / item.srcs[0].aspect_ratio}px`);
+        img.src = item.srcs[0].src;
+        img.setAttribute('style', `width: ${cardWidth}px; height: ${cardWidth / item.srcs[0].aspect_ratio}px;`);
+        
+    }else{
+        card.setAttribute('style', `position:absolute;left: ${item.x}px;top: ${item.y}px;width: ${cardWidth}px;height: ${cardWidth / item.aspect_ratio}px`);
+        img.src = item.src;
+        img.setAttribute('style', `width: ${cardWidth}px; height: ${cardWidth / item.aspect_ratio}px;`);
+        
+    }
+    
+   if (isTitle){ const title = document.createElement('h3');
     title.textContent = item.title;
     title.setAttribute('style', `position:absolute;font-size: ${cardWidth / 18}px;z-index:9;bottom:0;left:10px;color:white`);
 
     const div = document.createElement('div');
     div.setAttribute('style', `position:absolute;height:40px;left:0;right:0;bottom:-40px;overflow:hidden;background:#fff3;backdrop-filter:blur(10px);transition:all 0.3s ease `)
-    div.appendChild(title)
-   
-
-    const img = document.createElement('img');
-    img.src = item.srcs[0].src;
-    img.setAttribute('style', `width: ${cardWidth}px; height: ${cardWidth / item.srcs[0].aspect_ratio}px;`);
-
-    card.appendChild(img);
+    div.appendChild(title)  
     card.appendChild(div);
-
     card.addEventListener('mouseover', () => {
         div.style.bottom = '0px';
     })
     card.addEventListener('mouseout', () => {
         div.style.bottom = '-40px';
     })
+}
+
+    
+  
+
+    card.appendChild(img);
     return card 
 }
 
@@ -147,10 +196,6 @@ function loadData() {
 
 window.addEventListener('DOMContentLoaded', init);
 
-// window.addEventListener('resize',()=>{
-//     app.innerHTML = '';
-//     debounce(init, 100)()
-// })
 
 //防抖
 function debounce(func, wait) {
@@ -163,4 +208,10 @@ function debounce(func, wait) {
             func.apply(context, args);
         }, wait);
     };
+}
+
+
+function scendFolder(img) {
+    document.createElement('div')
+
 }
